ff754769236f019018655edcc874fe6a355406c3
Frank Lemanschik <frank@lemanschik.com>
2023-05-30 10:33:40 +0000

    30. Mai 2023, 12:33

--- origin.js
+++ origin.js
@@ -1,14 +1,2 @@
 globalThis.navigator && globalThis.navigator.serviceWorker && globalThis?.navigator?.serviceWorker?.register('origin.service-worker.js');
-globalThis.sharedWorkers = globalThis.sharedWorkers || {};
-const importUrl = new URL('origin.shared-worker.js',import.meta.url)
-globalThis.sharedWorkers[importUrl] = (readable,launch) => {
-    const processor = new SharedWorker(importUrl);
-    processor.postMessage({ launch });
-    readable.pipeTo(new WritableStream({ write(input){ 
-        processor.postMessage(input);
-    }}));
-    return new ReadableStream({ start(output){ 
-        processor.onmessage = (watch) => output.enqueue(watch);
-    }, close(){processor.close();}});
-};
 export default globalThis;
