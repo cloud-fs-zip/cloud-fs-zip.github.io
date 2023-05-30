2cd23f36650493c20e6b0f554523a25d340e95cb
Frank Lemanschik <frank@lemanschik.com>
2023-05-30 10:36:06 +0000

    30. Mai 2023, 12:36

--- origin.service-worker.js
+++ origin.service-worker.js
@@ -1,4 +1,19 @@
 // service worker for the current scope
 import serviceWorker from './origin.js';
-import * as Comlink from "unpkg/comlink/dist/esm/comlink.mjs";
-serviceWorker.onfetch()
+
+
+
+serviceWorker.sharedWorkers = globalThis.sharedWorkers || {};
+const importUrl = new URL('origin.shared-worker.js',import.meta.url)
+serviceWorker.sharedWorkers[importUrl] = (launch,readable) => {
+    const processor = new SharedWorker(importUrl);
+    processor.postMessage({ launch });
+    readable.pipeTo(new WritableStream({ write(input){ 
+        processor.postMessage(input);
+    }}));
+    return new ReadableStream({ start(output){ 
+        processor.onmessage = (watch) => output.enqueue(watch);
+    }, close(){processor.close();}});
+};
+
+serviceWorker.onfetch = (request)=>serviceWorker.sharedWorkers[importUrl]()
