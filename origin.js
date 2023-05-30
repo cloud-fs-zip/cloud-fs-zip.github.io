974cb9aee19424ad51df44837204adba629e313b
Frank Lemanschik <frank@lemanschik.com>
2023-05-30 09:50:26 +0000

    30. Mai 2023, 11:50

--- origin.js
+++ origin.js
@@ -1,5 +1,5 @@
 globalThis.navigator && globalThis.navigator.serviceWorker && globalThis?.navigator?.serviceWorker?.register('origin.service-worker.js');
 globalThis.sharedWorkers = globalThis.sharedWorkers || {};
 const importUrl = new URL('origin.shared-worker.js',import.meta.url)
-globalThis.sharedWorkers[importUrl] =  new SharedWorker(importUrl);
+globalThis.sharedWorkers[importUrl] = () => new SharedWorker(importUrl);
 export default globalThis;
