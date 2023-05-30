36e79cc433f4c35007c68f0e1be93b7e48570722
Frank Lemanschik <frank@lemanschik.com>
2023-05-30 08:24:51 +0000

    30. Mai 2023, 10:24

--- origin.shared-worker.js
+++ origin.shared-worker.js
@@ -3,13 +3,13 @@
 // service worker for the current scope
 const sharedWorker = origin.sharedWorkers[import.meta.url];
 
-sharedWorker.onconnect = ({ports:[port]}) => port.onmessage = ({data: {id, run }}) => {
+sharedWorker.onconnect = ({ports:[port]}) => port.onmessage = ({data: { id, run }}) => {
     if (id && run) {
         new ReadableStream({ async start(progress){ 
             // the function is able to be a readable stream or iterator 
             // you should always return { stdout, stderr or one of them.}
-            for await (const value of [].concat(await (new Function(`return ${run}`))(port))) {
-                progress.enqueue(value)
+            for await (const output of [].concat(await (new Function(`return ${run}`))(port))) {
+                progress.enqueue({ id, output })
             }
         }}).pipeTo(new WritableStream({write(progress){
             port.postMessage(progress)
