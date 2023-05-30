d5edf8dbe49133494c10a4d2a1cf2841c6f079a4
Frank Lemanschik <frank@lemanschik.com>
2023-05-30 08:04:14 +0000

    30. Mai 2023, 10:04

--- origin.shared-worker.js
+++ origin.shared-worker.js
@@ -1,8 +1,9 @@
 // Serializes messages cross context via BroadcastChannel output merge to readableStream
 // Represents the Runtime State Context 
 // service worker for the current scope
-import sharedWorker from './origin.js';
 import * as Comlink from "unpkg/comlink/dist/esm/comlink.mjs";
-serviceWorker.onfetch()
-sharedWorker.sharedWorkers[import.meta.url]
-Comlink.expose(serviceWorker)
+
+const sharedWorker = origin.sharedWorkers[import.meta.url];
+
+
+Comlink.expose(sharedWorker)
