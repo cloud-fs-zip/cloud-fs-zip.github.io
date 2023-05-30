b7bf2c4ae63f240150a752d5b54d9e596e521b7f
Frank Lemanschik <frank@lemanschik.com>
2023-05-30 08:23:44 +0000

    30. Mai 2023, 10:23

--- origin.shared-worker.js
+++ origin.shared-worker.js
@@ -1,14 +1,13 @@
 // Serializes messages cross context via BroadcastChannel output merge to readableStream
 // Represents the Runtime State Context 
 // service worker for the current scope
-import * as Comlink from "unpkg/comlink/dist/esm/comlink.mjs";
-
 const sharedWorker = origin.sharedWorkers[import.meta.url];
 
 sharedWorker.onconnect = ({ports:[port]}) => port.onmessage = ({data: {id, run }}) => {
     if (id && run) {
         new ReadableStream({ async start(progress){ 
             // the function is able to be a readable stream or iterator 
+            // you should always return { stdout, stderr or one of them.}
             for await (const value of [].concat(await (new Function(`return ${run}`))(port))) {
                 progress.enqueue(value)
             }
@@ -16,9 +15,4 @@ sharedWorker.onconnect = ({ports:[port]}) => port.onmessage = ({data: {id, run }
             port.postMessage(progress)
         }}))
     }
-    
-
 }
-
-sharedWorker.run = (promise,port) => 
-Comlink.expose(sharedWorker)
