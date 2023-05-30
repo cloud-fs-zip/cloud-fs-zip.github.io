f83b6da0af40bbd5df69ea47a4645f8bd308bebf
Frank Lemanschik <frank@lemanschik.com>
2023-05-30 08:15:50 +0000

    30. Mai 2023, 10:15

--- origin.shared-worker.js
+++ origin.shared-worker.js
@@ -5,10 +5,15 @@ import * as Comlink from "unpkg/comlink/dist/esm/comlink.mjs";
 
 const sharedWorker = origin.sharedWorkers[import.meta.url];
 
-sharedWorker.run = (promise) => new ReadableStream({ async start(progress){ 
+sharedWorker.onconnect = ({ports:[port]}) => port.onmessage = ({data: {id, run }}) => {
+
+}
+
+sharedWorker.run = (promise,port) => new ReadableStream({ async start(progress){ 
     for await (const value of await (new Function(`return ${promise}`))()) {
         progress.enqueue(value);
     }
-    
-}})
+}}).pipeTo(new WritableStream({write(progress){
+    port.postMessage(progress)
+}}))
 Comlink.expose(sharedWorker)
