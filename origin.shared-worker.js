50b71c395775bb90410a12d0a59c01ce468d963a
Frank Lemanschik <frank@lemanschik.com>
2023-05-30 09:11:30 +0000

    30. Mai 2023, 11:11

--- origin.shared-worker.js
+++ origin.shared-worker.js
@@ -11,25 +11,32 @@ const readEverything =     new ReadableStream({ async start(progress){
         progress.enqueue({ id, output })
     }
 }});
+
+({ data: { run, ...data }}) => {
+                if (run) {
+                    try {
+                    // sharedWorker.tasks[id].readable //.pipeThough(new TransformStream({transform(watch,observer){ observer.enqueue(watch); }}))
+                    // .pipeTo(sharedWorker.tasks[id].writable);
+                    } catch(stderr) {
+                        port.postMessage({ id, output: { stderr }})
+                    }
+                }
 */
-sharedWorker.tasks = [];
+
 sharedWorker.onconnect = ({ports:[port]}) => {
-    port.id = crypto.randomUUID()
-    port.onmessage = ({ data: { run, data }}) => {
-    if (run) {
-        try {
-        sharedWorker.tasks[id] = { 
-            readable: new Function(`return ${run}`)(port), 
-            writable: new WritableStream({write(output){
+    port.id = crypto.randomUUID();
+    const run = { 
+        readable: new ReadableStream({ start(stdin) {
+            port.onmessage = (input) => stdin.enqueue(input);
+        }}), 
+        transform: new Function(`return ${run}`)(port),
+        writable: new WritableStream({write(output){
             // the receiver destructures id, { stderr, stdout } = output
             port.postMessage({ id, output });
             }}),
         };
-        sharedWorker.tasks[id].readable //.pipeThough(new TransformStream({transform(watch,observer){ observer.enqueue(watch); }}))
-        .pipeTo(sharedWorker.tasks[id].writable);
-        } catch(stderr) {
-            port.postMessage({ id, output: { stderr }})
-        }
-    }
+
+    port.onmessage = ({ data: { run, ...data }}) => {
+    
     }
 }
