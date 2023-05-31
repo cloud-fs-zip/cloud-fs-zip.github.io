13250939c658f2ea0dd59ec13cbc78ca4168ec0d
Frank Lemanschik <frank@lemanschik.com>
2023-05-31 08:31:39 +0000

    31. Mai 2023, 10:31

--- explorer/components/workbench/index.js
+++ explorer/components/workbench/index.js
@@ -2,7 +2,7 @@
 // npm install --prefix $PWD -g vscode-web
 
 require.config({
-	baseUrl: `${new URL('.',import.meta.url)}/lib/node_modules/vscode-web/dist/out`,
+	baseUrl: `${new URL('.',document.location)}/lib/node_modules/vscode-web/dist/out`,
 	recordStats: true,
 	trustedTypesPolicy: window.trustedTypes?.createPolicy('amdLoader', {
 		createScriptURL(value) {
@@ -10,7 +10,7 @@ require.config({
 		}
 	}),
 	paths: Object.entries(self.webPackagePaths).map(function ([key, value]) {
-		return [key,`${new URL('.',import.meta.url)}/lib/node_modules/vscode-web/dist/node_modules/${key}/${value}`];
+		return [key,`${new URL('.',document.location)}/lib/node_modules/vscode-web/dist/node_modules/${key}/${value}`];
 	}),
 });
 /* 
