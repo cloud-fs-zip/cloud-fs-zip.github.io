c09b2896b274515f1e959320f495630013331372
Frank Lemanschik <frank@lemanschik.com>
2023-06-01 07:21:06 +0000

    1. Juni 2023, 09:21

--- code/code.js
+++ code/code.js
@@ -1,10 +1,10 @@
 //import('./module.js')
 // mkdir node_modules && npm install --prefix $PWD vscode-web
 Object.entries(self.webPackagePaths).map(([key, value]) => 
-self.webPackagePaths[key] = `${new URL(`/node_modules/vscode-web/dist/node_modules/${key}/${value}`,window.location)}`);
+self.webPackagePaths[key] = `${new URL(`./node_modules/vscode-web/dist/node_modules/${key}/${value}`,window.location)}`);
 console.log(self.webPackagePaths)
 	require.config({
-	baseUrl: `${new URL('/node_modules/vscode-web/dist/out/',window.location)}`,
+	baseUrl: `${new URL('./node_modules/vscode-web/dist/out/',window.location)}`,
 	recordStats: true,
 	trustedTypesPolicy: window.trustedTypes?.createPolicy('amdLoader', {
 		createScriptURL(value) {
