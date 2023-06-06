ec904e72eddef313e32e992cf826fabe4dc1c14d
Frank Lemanschik <frank@lemanschik.com>
2023-06-06 07:39:59 +0000

    6. Juni 2023, 09:39

--- extensions/browser/opfs/index.js
+++ extensions/browser/opfs/index.js
@@ -1,34 +1,35 @@
-const html = (str) => str
-window.addEventListener("DOMContentLoaded", (event) => {
-    const worker = `
-    const getRandomValues = () => {
-      return Array.from({length: 50}, () => Math.floor(Math.random() * 50)).join('');
-    };
-    
-    (async () => {
-      const root = await navigator.storage.getDirectory();
-      for (let i = 0; i < 10; i++) {
-        const directoryHandle = await root.getDirectoryHandle('directory' + i, {create: true});
-        for (let j = 0; j < 10; j++) {
-          const fileHandle = await directoryHandle.getFileHandle('file' + j + '.txt', {create: true});
-          const writable = await fileHandle.createWritable();
-          await writable.write('Hello, world ' + i + '/' + j + '!\\n\\n' + getRandomValues());
-          await writable.close();
-        }
-      }
-    })();`;
-    const blob = new Blob([worker], { type: "text/javascript" });
-    const url = URL.createObjectURL(blob);
-    new Worker(url);
-    document.body.innerHTML = `<style>
-    :root {
-      color-scheme: dark light;
-      font-family: system-ui, sans-serif;
+const html = (str) => str;
+window.addEventListener("DOMContentLoaded", (_event) => {
+const worker = `
+const getRandomValues = () => {
+    return Array.from({length: 50}, () => Math.floor(Math.random() * 50)).join('');
+};
+
+(async () => {
+    const root = await navigator.storage.getDirectory();
+    for (let i = 0; i < 10; i++) {
+    const directoryHandle = await root.getDirectoryHandle('directory' + i, {create: true});
+    for (let j = 0; j < 10; j++) {
+        const fileHandle = await directoryHandle.getFileHandle('file' + j + '.txt', {create: true});
+        const writable = await fileHandle.createWritable();
+        await writable.write('Hello, world ' + i + '/' + j + '!\\n\\n' + getRandomValues());
+        await writable.close();
     }
-  </style>
-    <h1>OPFS Explorer Demo</h1>
-    <p>
-      This demo creates an OPFS file hierarchy for testing the extension. Use
-      the OPFS Explorer extension to inspect it.
-    </p>`;
+    }
+})();`;
+const blob = new Blob([worker], { type: "text/javascript" });
+const url = URL.createObjectURL(blob);
+new Worker(url);
+document.body.innerHTML = html`
+<style>
+:root {
+    color-scheme: dark light;
+    font-family: system-ui, sans-serif;
+}
+</style>
+<h1>OPFS Explorer Demo</h1>
+<p>
+    This demo creates an OPFS file hierarchy for testing the extension. Use
+    the OPFS Explorer extension to inspect it.
+</p>`;
 });
