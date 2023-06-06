00d8bf37327e6281a27a4c4d0a5e76f8c63ad32a
Frank Lemanschik <frank@lemanschik.com>
2023-06-06 07:36:08 +0000

    6. Juni 2023, 09:36

--- extensions/browser/opfs/index.js
+++ extensions/browser/opfs/index.js
@@ -1,25 +1,27 @@
-const worker = `
-const getRandomValues = () => {
-  return Array.from({length: 50}, () => Math.floor(Math.random() * 50)).join('');
-};
-
-(async () => {
-  const root = await navigator.storage.getDirectory();
-  for (let i = 0; i < 10; i++) {
-    const directoryHandle = await root.getDirectoryHandle('directory' + i, {create: true});
-    for (let j = 0; j < 10; j++) {
-      const fileHandle = await directoryHandle.getFileHandle('file' + j + '.txt', {create: true});
-      const writable = await fileHandle.createWritable();
-      await writable.write('Hello, world ' + i + '/' + j + '!\\n\\n' + getRandomValues());
-      await writable.close();
-    }
-  }
-})();`;
-const blob = new Blob([worker], { type: "text/javascript" });
-const url = URL.createObjectURL(blob);
-new Worker(url);
-document.body.innerHTML = `<h1>OPFS Explorer Demo</h1>
-<p>
-  This demo creates an OPFS file hierarchy for testing the extension. Use
-  the OPFS Explorer extension to inspect it.
-</p>`
+window.addEventListener("DOMContentLoaded", (event) => {
+    const worker = `
+    const getRandomValues = () => {
+      return Array.from({length: 50}, () => Math.floor(Math.random() * 50)).join('');
+    };
+    
+    (async () => {
+      const root = await navigator.storage.getDirectory();
+      for (let i = 0; i < 10; i++) {
+        const directoryHandle = await root.getDirectoryHandle('directory' + i, {create: true});
+        for (let j = 0; j < 10; j++) {
+          const fileHandle = await directoryHandle.getFileHandle('file' + j + '.txt', {create: true});
+          const writable = await fileHandle.createWritable();
+          await writable.write('Hello, world ' + i + '/' + j + '!\\n\\n' + getRandomValues());
+          await writable.close();
+        }
+      }
+    })();`;
+    const blob = new Blob([worker], { type: "text/javascript" });
+    const url = URL.createObjectURL(blob);
+    new Worker(url);
+    document.body.innerHTML = `<h1>OPFS Explorer Demo</h1>
+    <p>
+      This demo creates an OPFS file hierarchy for testing the extension. Use
+      the OPFS Explorer extension to inspect it.
+    </p>`;
+});
